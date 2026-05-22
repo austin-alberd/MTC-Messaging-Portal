f3dc8c9999bc4a53fa5abd7af2db4ebe37f43e0f
Austin Alberd <austinalberd2005@gmail.com>
2026-05-22 00:32:50 -0500

    ahhh

---
+++ frontend/script/loginlogout.js
@@ -0,0 +1,9 @@
+document.querySelector("#btnLogin").addEventListener("click",()=>{
+    // Some login logic needs to be put here >_<
+    showRegularNotificationPage()
+})
+
+document.querySelector("#btnLogOut").addEventListener("click",()=>{
+    //Some logic needs to go here >~<
+    showLoginPage()
+})
---
+++ frontend/script/tabhandler.js
@@ -0,0 +1,15 @@
+const navbar = document.querySelector("#navbar")
+const loginPage = document.querySelector("#loginPage")
+const notificationPageRegular = document.querySelector("#sendANotificationPage")
+
+function showLoginPage(){
+    navbar.style.display ="none"
+    notificationPageRegular.style.display = "none"
+    loginPage.style.display = "block"
+}
+
+function showRegularNotificationPage(){
+    navbar.style.display ="block"
+    notificationPageRegular.style.display = "block"
+    loginPage.style.display = "none"
+}
